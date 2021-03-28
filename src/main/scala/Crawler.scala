7a285b4b10a847543a533c5855439e06cb80cd8a
David Timms <david.timms@kaluza.com>
2021-03-28 18:40:07 +0100

    Recursively crawl links

--- src/main/scala/Crawler.scala
+++ src/main/scala/Crawler.scala
@@ -1,13 +1,46 @@
+import cats.implicits._
 import cats.effect._
 import org.http4s._
-import org.http4s.implicits._
+import org.http4s.client.Client
 import org.http4s.client.blaze._
-import org.http4s.client._
+
 import scala.concurrent.ExecutionContext.global
 
-case class Crawler() {
-  def crawl(pageUri: Uri): IO[WebPage] =
-    BlazeClientBuilder[IO](global).resource.use { httpClient =>
-      httpClient.expect[String](pageUri).map(WebPage.fromHtml)
-    }
+case class Crawler(maxDepth: Int) {
+  def crawl(startingUri: String): IO[WebPage] = {
+    for {
+      parsedUri <- Uri.fromString(startingUri) match {
+        case Left(_) => IO.raiseError(new Error("Invalid starting URI"))
+        case Right(uri) => IO.pure(uri)
+      }
+      page <- BlazeClientBuilder[IO](global).resource.use { httpClient =>
+        crawlPage(httpClient, parsedUri)
+      }
+    } yield page
+  }
+
+  def crawlPage(httpClient: Client[IO], pageUri: Uri, depth: Int = 0): IO[WebPage] = {
+    // TODO follow redirects
+    // TODO handle failed requests
+    for {
+      page <- httpClient.expect[String](pageUri).map(WebPage.fromHtml)
+      _ <- IO.println("  ".repeat(depth) + "- " + page.title.getOrElse("(no title)"))
+      links = crawlableLinks(page)
+      _ <- {
+        if (depth < maxDepth)
+          links.parTraverse { uri => crawlPage(httpClient, uri, depth + 1) }
+        else
+          IO.unit
+      }
+
+    } yield page
+  }
+
+  private def crawlableLinks(webPage: WebPage): List[Uri] =
+    webPage
+      .links
+      .map(Uri.fromString)
+      .flatMap(_.toSeq)
+      // TODO convert relative links to absolute
+      .filter(uri => uri.scheme.isDefined)
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,20 +3,14 @@ import cats.effect._
 import org.http4s._
 
 object Main extends IOApp {
-  def run(args: List[String]): IO[ExitCode] = {
+  // TODO take max-depth as an argument
+  def run(args: List[String]): IO[ExitCode] =
     args.headOption match {
       case Some(startingUri) =>
-        for {
-          parsedUri <- Uri.fromString(startingUri) match {
-            case Left(parseFailure) => IO.raiseError(new Error("Invalid starting URI"))
-            case Right(uri) => IO.pure(uri)
-          }
-          webPage <- Crawler().crawl(parsedUri)
-          _ <- IO.println(webPage)
-        } yield ExitCode.Success
+        Crawler(maxDepth = 2).crawl(startingUri)
+          .as(ExitCode.Success)
       case None =>
         IO(Console.err.println("ERROR: No starting URI provided"))
           .as(ExitCode.Error)
     }
-  }
 }
